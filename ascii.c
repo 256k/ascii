bde74f3f21b54a4cf9851c326d74b610ca6de079
256k <fayek.helmi@gmail.com>
2024-02-20 20:53:12 -0500

    added special character conditions

--- ascii.c
+++ ascii.c
@@ -12,13 +12,55 @@ by 256k
 
 #include <stdio.h>
 
+char *SPECIAL_CHARS[] = {
+"NUL", 
+"SOH", 
+"STX", 
+"ETX", 
+"EOT", 
+"ENQ", 
+"ACK", 
+"BEL", 
+"BS ", 
+"TAB", 
+"LF ", 
+"VT ", 
+"FF ", 
+"CR ", 
+"SO ", 
+"SI ", 
+"DLE", 
+"DC1", 
+"DC2", 
+"DC3", 
+"DC4", 
+"NAK", 
+"SYN", 
+"ETB", 
+"CAN", 
+"EM ", 
+"SUB", 
+"ESC", 
+"FS ", 
+"GS ", 
+"RS ", 
+"US ",
+"SPC"
+}; 
+
 int main(int argc, char **argv)
 {
         if (!argv[1])
         {
                 for (int i = 0; i < 128; i++) 
                 {
+                        if (i < 33 ) {
+                                printf("%d \t %x \t %s\n", i,i,SPECIAL_CHARS[i]);
+                        } else if (i == 127) {
+                        printf("%d \t %x \t %s\n", i,i,"DEL");
+                        } else {
                         printf("%d \t %x \t %c\n", i,i,i);
+                        }
                 }        
         } else 
         { 
